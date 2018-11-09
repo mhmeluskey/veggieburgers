e0ebbc7231d40d948f97dad1ee093c5f450bb674
Megan <mhmeluskey@gmail.com>
2018-11-09 11:48:41 -0500

    make it better

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,16 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+  
 
+var connection = mysql.createConnection({
+  host: "process.env.JAWSDB_URL",
+  port: 8080,
+  user: "root",
+  password: "password",
+  database: "burgers_db"
+});
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
+// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -11,19 +18,6 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
-
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-  
-
-
-// Make connection.
 
 // Export connection for our ORM to use.
 module.exports = connection;
