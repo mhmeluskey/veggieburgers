8879ecff218941be90197fcafac4f41e9ce377a2
Megan <mhmeluskey@gmail.com>
2018-11-19 19:34:06 -0500

    style changes to divs

--- config/connection.js
+++ config/connection.js
@@ -3,22 +3,22 @@ var mysql = require('mysql');
 
 
 	// DB is local on localhost
-	var connection = mysql.createConnection({
-		port: 3306,
-		host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'	,
-		user: 'p34sfrpw64jox535',
-		password: 't1rz28133ef7hvqf',
-		database: 'lool3mgfdor8vd1z'
-	});
-
-
 	// var connection = mysql.createConnection({
-	// 	host: "localhost",
 	// 	port: 3306,
-	// 	user: "root",
-	// 	password: "password",
-	// 	database: "burgers_db"
+	// 	host: 'kavfu5f7pido12mr.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'	,
+	// 	user: 'p34sfrpw64jox535',
+	// 	password: 't1rz28133ef7hvqf',
+	// 	database: 'lool3mgfdor8vd1z'
 	// });
+
+
+	var connection = mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "password",
+		database: "burgers_db"
+	});
 
 // Make the connection to MySQL
 connection.connect(function(err) {
